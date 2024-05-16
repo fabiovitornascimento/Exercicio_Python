783c05519c64d44b45c02731d49bd0820e721b02
fabiovitornascimento <fabiovitornascimento@outlook.com>
2024-05-16 09:26:09 -0300

    Primeiro exercício

--- Criando um Sistema bancário.py
+++ Criando um Sistema bancário.py
@@ -1,4 +1,5 @@
-menu = """\n[1] Depositar
+menu = """
+[1] Depositar
 [2] Sacar
 [3] Extrato
 [0] Sair
@@ -9,7 +10,7 @@ limite = 500
 extrato = []
 numero_saques = 0
 limite_saques = 3
-extrato.append(f" Saldo: R${saldo}")
+extrato.append(f"Saldo: R${saldo}")
 
 while True:
 
@@ -17,25 +18,25 @@ while True:
 
     if opcao == "1":
         print("\n Depósito")
-        valor = float(input(" Qual valor você deseja depositar hoje? R$"))
+        valor = float(input("Qual valor você deseja depositar hoje? R$"))
         if valor <= 0:
             print("\nValores negativos não são aceito!")
         else:
             saldo += valor
-            extrato.append(f" Deposito: R${valor}")
-            extrato.append(f" Saldo R${saldo}")
+            extrato.append(f"Deposito: R${valor}")
+            extrato.append(f"Saldo R${saldo}")
         print(f"\nVocê depositou R${valor}")
 
     elif opcao == "2":
-        print("\nSaque")
+        print("\n Saque")
         print("Limite de R$500 reais por saque, e só é permitido 3 saques por dia")
         saque = float(input("Qual valor você deseja sacar? R$"))
         if saque <= saldo and saque <= 500:
             if limite_saques != numero_saques:
                 numero_saques += 1
                 saldo -= saque
-                extrato.append(f" Você sacou -R${saque} reais")
-                extrato.append(f" Saldo R${saldo}")
+                extrato.append(f"Você sacou -R${saque} reais")
+                extrato.append(f"Saldo R${saldo}")
                 print(f"\nVocê sacou R${saque} reais, e agora seu saldo ficou R${saldo} reais")
             else:
                 print("\nNúmero de Saque diario atingido!, volte novamente amanhã")
@@ -45,7 +46,7 @@ while True:
             print("\n Operação cancelada, saque maior que o valor permitido")
 
     elif opcao == "3":
-        print("Extrato")
+        print("\n Extrato")
         for desmontrativo in extrato:
             print(desmontrativo)
 
